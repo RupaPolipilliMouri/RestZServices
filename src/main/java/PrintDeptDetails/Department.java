1e2157e0c88d1d6b9c4efc002a3cf9972ddf9f9b
RupaPolipilliMouri <96765255+RupaPolipilliMouri@users.noreply.github.com>
2022-01-22 14:01:00 +0530

    Add files via upload

---
+++ src/main/java/PrintDeptDetails/Department.java
@@ -0,0 +1,41 @@
+package PrintDeptDetails;
+
+public class Department 
+{
+	
+private int deptid;
+private String deptName;
+
+
+public Department()
+{
+	
+}
+public Department(int deptid, String deptName) {
+	super();
+	this.deptid = deptid;
+	this.deptName = deptName;
+}
+public int getDeptid() {
+	return deptid;
+}
+public void setDeptid(int deptid) {
+	this.deptid = deptid;
+}
+public String getDeptName() {
+	return deptName;
+}
+public void setDeptName(String deptName) {
+	this.deptName = deptName;
+}
+
+
+@Override
+public String toString() {
+	return "Department [deptid=" + deptid + ", deptName=" + deptName + "]";
+}
+
+
+
+
+}
---
+++ src/main/java/com/mouritech/helloworldservice/HelloWorldRestServiceExample.java
@@ -0,0 +1,17 @@
+package com.mouritech.helloworldservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/hello")
+
+public class HelloWorldRestServiceExample
+{
+
+	   //@Path("/printhello")
+	   @GET
+	   public String printHello() {
+		   return "Hello From the Rest Service";
+	   }
+
+}
